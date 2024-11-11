7a0c72c411e7d5da0e364d10ea9ed7c93be10ef5
kranti4 <146917885+kranti4@users.noreply.github.com>
2024-11-11 13:15:18 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
-
-#-f Fail silently (no output at all) on server errors.
-#s--silent Silent or quiet mode. Don't show progress meter or error messages.
-#-S, --show-error and when used with -s , it will show an error
-#-L, --location If the server reports that the requested page has moved to a different location
-#A System/software update is typically a release containing enhancements to the current version. 
-#An upgrade is a whole new version of software that represents a significant change or major improvement.
-#DEB - A deb package (.deb file) is a software package in a specific format designed for Debian-based distributions recognized by the .deb extension. \
-#Deb packages allow installing local software on an Ubuntu system.
-
-
-
-
-
 sudo apt update -y
-
-sudo apt upgrade -y 
-
-sudo apt install openjdk-17-jre -y
-
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y 
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+sudo apt update -y
+sudo apt install temurin-17-jdk -y
+/usr/bin/java --version
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
 sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
